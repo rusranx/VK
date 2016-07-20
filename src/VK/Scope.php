5bc071efa7719e7e3b0268b6d3f08a84185e89ca
rusran4 <rusran4@ya.ru>
2016-07-21 03:22:33 +0400

    Added Scope && RusranUtils[Curl] support

---
+++ src/VK/Scope.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Scope
+ *
+ * @project VK
+ *
+ * @author  Yuri Ashurkov (rusranx)
+ * @license MIT
+ */
+
+namespace VK;
+
+
+class Scope
+{
+	const
+		NOTIFY = 1 << 0,            // Пользователь разрешил отправлять ему уведомления (для flash/iframe-приложений).
+		FRIENDS = 1 << 1,           // Доступ к друзьям.
+		PHOTOS = 1 << 2,            // Доступ к фотографиям.
+		AUDIO = 1 << 3,             // Доступ к аудиозаписям.
+		VIDEO = 1 << 4,             // Доступ к видеозаписям.
+		OFFERS = 1 << 5,            // Доступ к предложениям (устаревшие методы).
+		QUESTIONS = 1 << 6,         // Доступ к вопросам (устаревшие методы).
+		PAGES = 1 << 7,             // Доступ к wiki-страницам.
+		MENU = 1 << 8,              // Добавление ссылки на приложение в меню слева.
+		UNKNOWN_512 = 1 << 9,
+		STATUS = 1 << 10,           // Доступ к статусу пользователя.
+		NOTES = 1 << 11,            // Доступ к заметкам пользователя.
+		MESSAGES = 1 << 12,         // Доступ к расширенным методам работы с сообщениями.
+		WALL = 1 << 13,             // Доступ к стене
+		UNKNOWN_16384 = 1 << 14,
+		ADS = 1 << 15,              // Доступ к расширенным методам работы с рекламным API.
+		OFFLINE = 1 << 16,          // Доступ к API в любое время со стороннего сервера (при использовании этой опции параметр expires_in, возвращаемый вместе с access_token, содержит 0 — токен бессрочный).
+		DOCS = 1 << 17,             // Доступ к документам.
+		GROUPS = 1 << 18,           // Доступ к группам пользователя.
+		NOTIFICATIONS = 1 << 19,    // Доступ к оповещениям об ответах пользователю.
+		STATS = 1 << 20,            // Доступ к статистике групп и приложений пользователя, администратором которых он является.
+		UNKNOWN_2097152 = 1 << 21,
+		EMAIL = 1 << 22,            // Доступ к email пользователя.
+		ADSCABINET = 1 << 23,       // Доступ к кабинетам рекламной сети
+		UNKNOWN_16777216 = 1 << 24,
+		UNKNOWN_33554432 = 1 << 25,
+		EXCHANGE = 1 << 26,         // Доступ к кабинетам биржи рекламы ВКонтакте
+		MARKET = 1 << 27;           // Доступ к товарам
+
+	/**
+	 * Возвращает всемогущий Scope
+	 *
+	 * @return int
+	 */
+	public static function getAll()
+	{
+		$constants = (new \ReflectionClass(__CLASS__))->getConstants();
+		$scope = 0;
+		foreach ($constants as $constant => $value) {
+			$scope |= $value;
+		}
+
+		return $scope;
+	}
+
+	private $_scope = 0;
+
+	/**
+	 * Добавление доступ к чему-либо
+	 *
+	 * @param array|string $permissionNames
+	 * @return Scope $this
+	 */
+	public function addPermission($permissionNames)
+	{
+		if (is_array($permissionNames)) {
+			foreach ($permissionNames as $permissionName)
+				$this->addPermission($permissionName);
+		} else {
+			$permission = strtoupper(trim($permissionNames));
+			if (strpos($permission, ',') !== false) {
+				$permissionNames = explode(',', $permission);
+				$this->addPermission($permissionNames);
+			} else {
+				if (isset($this->$permission)) {
+					$this->_scope |= $this->$permission;
+				}
+			}
+		}
+
+		return $this;
+	}
+
+	public function get()
+	{
+		return $this->_scope;
+	}
+
+	public function __toString()
+	{
+		return (string)$this->_scope;
+	}
+
+}
--- src/VK/VK.php
+++ src/VK/VK.php
@@ -2,264 +2,313 @@
 
 /**
  * The PHP class for vk.com API and to support OAuth.
- * @author Vlad Pronsky <vladkens@yandex.ru>
+ *
+ * @author  Vlad Pronsky <vladkens@yandex.ru>
  * @license https://raw.github.com/vladkens/VK/master/LICENSE MIT
  */
 
 namespace VK;
 
+use RusranUtils\Curl;
+
 class VK
 {
-    /**
-     * VK application id.
-     * @var string
-     */
-    private $app_id;
+	/**
+	 * VK application id.
+	 *
+	 * @var string
+	 */
+	private $app_id;
 
-    /**
-     * VK application secret key.
-     * @var string
-     */
-    private $api_secret;
+	/**
+	 * VK application secret key.
+	 *
+	 * @var string
+	 */
+	private $api_secret;
 
-    /**
-     * API version. If null uses latest version.
-     * @var int
-     */
-    private $api_version;
+	/**
+	 * API version. If null uses latest version.
+	 *
+	 * @var int
+	 */
+	private $api_version;
 
-    /**
-     * VK access token.
-     * @var string
-     */
-    private $access_token;
+	/**
+	 * VK access token.
+	 *
+	 * @var string
+	 */
+	private $access_token;
 
-    /**
-     * Authorization status.
-     * @var bool
-     */
-    private $auth = false;
+	/**
+	 * Authorization status.
+	 *
+	 * @var bool
+	 */
+	private $auth = false;
 
-    /**
-     * Instance curl.
-     * @var Resource
-     */
-    private $ch;
+	/**
+	 * Instance curl.
+	 *
+	 * @var Resource
+	 */
+	private $ch;
 
-    const AUTHORIZE_URL = 'https://oauth.vk.com/authorize';
-    const ACCESS_TOKEN_URL = 'https://oauth.vk.com/access_token';
+	const AUTHORIZE_URL = 'https://oauth.vk.com/authorize';
+	const ACCESS_TOKEN_URL = 'https://oauth.vk.com/access_token';
+	const API_URL = 'https://api.vk.com/method';
 
-    /**
-     * Constructor.
-     * @param   string $app_id
-     * @param   string $api_secret
-     * @param   string $access_token
-     * @throws  VKException
-     */
-    public function __construct($app_id, $api_secret, $access_token = null)
-    {
-        $this->app_id = $app_id;
-        $this->api_secret = $api_secret;
-        $this->setAccessToken($access_token);
+	/**
+	 * Constructor.
+	 *
+	 * @param   string $app_id
+	 * @param   string $api_secret
+	 * @param   string $access_token
+	 * @throws  VKException
+	 */
+	public function __construct($app_id, $api_secret, $access_token = null)
+	{
+		$this->app_id = $app_id;
+		$this->api_secret = $api_secret;
+		$this->setAccessToken($access_token);
 
-        $this->ch = curl_init();
-    }
+		$this->ch = new Curl();
+	}
 
-    /**
-     * Destructor.
-     */
-    public function __destruct()
-    {
-        curl_close($this->ch);
-    }
+	/**
+	 * Set special API version.
+	 *
+	 * @param   int $version
+	 * @return  void
+	 */
+	public function setApiVersion($version)
+	{
+		$this->api_version = $version;
+	}
 
-    /**
-     * Set special API version.
-     * @param   int $version
-     * @return  void
-     */
-    public function setApiVersion($version)
-    {
-        $this->api_version = $version;
-    }
+	/**
+	 * Set Access Token.
+	 *
+	 * @param   string $access_token
+	 * @throws  VKException
+	 * @return  void
+	 */
+	public function setAccessToken($access_token)
+	{
+		$this->access_token = $access_token;
+	}
 
-    /**
-     * Set Access Token.
-     * @param   string $access_token
-     * @throws  VKException
-     * @return  void
-     */
-    public function setAccessToken($access_token)
-    {
-        $this->access_token = $access_token;
-    }
+	/**
+	 * Returns base API url.
+	 *
+	 * @param   string $method
+	 * @param   string $response_format
+	 * @return  string
+	 */
+	public function getApiUrl($method, $response_format = 'json')
+	{
+		return sprintf("%s/%s.%s", self::API_URL, $method, $response_format);
+	}
 
-    /**
-     * Returns base API url.
-     * @param   string $method
-     * @param   string $response_format
-     * @return  string
-     */
-    public function getApiUrl($method, $response_format = 'json')
-    {
-        return 'https://api.vk.com/method/' . $method . '.' . $response_format;
-    }
+	/**
+	 * Returns authorization link with passed parameters.
+	 *
+	 * @param   string $api_settings
+	 * @param   string $callback_url
+	 * @param   bool   $test_mode
+	 * @return  string
+	 */
+	public function getAuthorizeUrl($api_settings = '', $callback_url = 'https://api.vk.com/blank.html', $test_mode = false)
+	{
+		$parameters = [
+			'client_id'     => $this->app_id,
+			'scope'         => $api_settings,
+			'redirect_uri'  => $callback_url,
+			'response_type' => 'code'
+		];
 
-    /**
-     * Returns authorization link with passed parameters.
-     * @param   string $api_settings
-     * @param   string $callback_url
-     * @param   bool $test_mode
-     * @return  string
-     */
-    public function getAuthorizeUrl($api_settings = '',
-                                    $callback_url = 'https://api.vk.com/blank.html', $test_mode = false)
-    {
-        $parameters = array(
-            'client_id' => $this->app_id,
-            'scope' => $api_settings,
-            'redirect_uri' => $callback_url,
-            'response_type' => 'code'
-        );
+		if ($test_mode)
+			$parameters['test_mode'] = 1;
 
-        if ($test_mode)
-            $parameters['test_mode'] = 1;
+		return $this->createUrl(self::AUTHORIZE_URL, $parameters);
+	}
 
-        return $this->createUrl(self::AUTHORIZE_URL, $parameters);
-    }
+	/**
+	 * Returns access token by code received on authorization link.
+	 *
+	 * @param   string $code
+	 * @param   string $callback_url
+	 * @throws  VKException
+	 * @return  array
+	 */
+	public function getAccessToken($code, $callback_url = 'https://api.vk.com/blank.html')
+	{
+		if (!is_null($this->access_token) && $this->auth) {
+			throw new VKException('Already authorized.');
+		}
 
-    /**
-     * Returns access token by code received on authorization link.
-     * @param   string $code
-     * @param   string $callback_url
-     * @throws  VKException
-     * @return  array
-     */
-    public function getAccessToken($code, $callback_url = 'https://api.vk.com/blank.html')
-    {
-        if (!is_null($this->access_token) && $this->auth) {
-            throw new VKException('Already authorized.');
-        }
+		$parameters = [
+			'client_id'     => $this->app_id,
+			'client_secret' => $this->api_secret,
+			'code'          => $code,
+			'redirect_uri'  => $callback_url
+		];
 
-        $parameters = array(
-            'client_id' => $this->app_id,
-            'client_secret' => $this->api_secret,
-            'code' => $code,
-            'redirect_uri' => $callback_url
-        );
+		$rs = json_decode($this->request(
+			$this->createUrl(self::ACCESS_TOKEN_URL, $parameters)), true);
 
-        $rs = json_decode($this->request(
-            $this->createUrl(self::ACCESS_TOKEN_URL, $parameters)), true);
+		if (isset($rs['error'])) {
+			throw new VKException($rs['error'] .
+				(!isset($rs['error_description']) ?: ': ' . $rs['error_description']));
+		} else {
+			$this->auth = true;
+			$this->access_token = $rs['access_token'];
 
-        if (isset($rs['error'])) {
-            throw new VKException($rs['error'] .
-                (!isset($rs['error_description']) ?: ': ' . $rs['error_description']));
-        } else {
-            $this->auth = true;
-            $this->access_token = $rs['access_token'];
-            return $rs;
-        }
-    }
+			return $rs;
+		}
+	}
 
-    /**
-     * Return user authorization status.
-     * @return  bool
-     */
-    public function isAuth()
-    {
-        return !is_null($this->access_token);
-    }
+	/**
+	 * Return user authorization status.
+	 *
+	 * @return  bool
+	 */
+	public function isAuth()
+	{
+		return !is_null($this->access_token);
+	}
 
-    /**
-     * Check for validity access token.
-     * @param   string $access_token
-     * @return  bool
-     */
-    public function checkAccessToken($access_token = null)
-    {
-        $token = is_null($access_token) ? $this->access_token : $access_token;
-        if (is_null($token)) return false;
+	/**
+	 * Check for validity access token.
+	 *
+	 * @param   string $access_token
+	 * @return  bool
+	 */
+	public function checkAccessToken($access_token = null)
+	{
+		$token = is_null($access_token) ? $this->access_token : $access_token;
+		if (is_null($token)) return false;
 
-        $rs = $this->api('getUserSettings', array('access_token' => $token));
-        return isset($rs['response']);
-    }
+		$rs = $this->api('getUserSettings', ['access_token' => $token]);
 
-    /**
-     * Execute API method with parameters and return result.
-     * @param   string $method
-     * @param   array $parameters
-     * @param   string $format
-     * @param   string $requestMethod
-     * @return  mixed
-     */
-    public function api($method, $parameters = array(), $format = 'array', $requestMethod = 'get')
-    {
-        $parameters['timestamp'] = time();
-        $parameters['api_id'] = $this->app_id;
-        $parameters['random'] = rand(0, 10000);
+		return isset($rs['response']);
+	}
 
-        if (!array_key_exists('access_token', $parameters) && !is_null($this->access_token)) {
-            $parameters['access_token'] = $this->access_token;
-        }
+	/**
+	 * Execute API method with parameters and return result.
+	 *
+	 * @param   string $method
+	 * @param   array  $parameters
+	 * @param   string $format
+	 * @param   string $requestMethod
+	 * @return  mixed
+	 */
+	public function api($method, $parameters = [], $format = 'array', $requestMethod = 'get')
+	{
+		$parameters['timestamp'] = time();
+		$parameters['api_id'] = $this->app_id;
+		$parameters['random'] = rand(0, 10000);
 
-        if (!array_key_exists('v', $parameters) && !is_null($this->api_version)) {
-            $parameters['v'] = $this->api_version;
-        }
+		if (!array_key_exists('access_token', $parameters) && !is_null($this->access_token)) {
+			$parameters['access_token'] = $this->access_token;
+		}
 
-        ksort($parameters);
+		if (!array_key_exists('v', $parameters) && !is_null($this->api_version)) {
+			$parameters['v'] = $this->api_version;
+		}
 
-        $sig = '';
-        foreach ($parameters as $key => $value) {
-            $sig .= $key . '=' . $value;
-        }
-        $sig .= $this->api_secret;
+		ksort($parameters);
 
-        $parameters['sig'] = md5($sig);
+		$sig = '';
+		foreach ($parameters as $key => $value) {
+			$sig .= $key . '=' . $value;
+		}
+		$sig .= $this->api_secret;
 
-        if ($method == 'execute' || $requestMethod == 'post') {
-            $rs = $this->request(
-                $this->getApiUrl($method, $format == 'array' ? 'json' : $format), "POST", $parameters);
-        } else {
-            $rs = $this->request($this->createUrl(
-                $this->getApiUrl($method, $format == 'array' ? 'json' : $format), $parameters));
-        }
-        return $format == 'array' ? json_decode($rs, true) : $rs;
-    }
+		$parameters['sig'] = md5($sig);
 
-    /**
-     * Concatenate keys and values to url format and return url.
-     * @param   string $url
-     * @param   array $parameters
-     * @return  string
-     */
-    private function createUrl($url, $parameters)
-    {
-        $url .= '?' . http_build_query($parameters);
-        return $url;
-    }
+		if ($method == 'execute' || $requestMethod == 'post') {
+			$rs = $this->request(
+				$this->getApiUrl($method, $format == 'array' ? 'json' : $format),
+				"POST",
+				$parameters
+			);
+		} else {
+			$rs = $this->request(
+				$this->createUrl(
+					$this->getApiUrl($method, $format == 'array' ? 'json' : $format),
+					$parameters
+				)
+			);
+		}
 
-    /**
-     * Executes request on link.
-     * @param   string $url
-     * @param   string $method
-     * @param   array $postfields
-     * @return  string
-     */
-    private function request($url, $method = 'GET', $postfields = array())
-    {
-        curl_setopt_array($this->ch, array(
-            CURLOPT_USERAGENT => 'VK/1.0 (+https://github.com/vladkens/VK))',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_POST => ($method == 'POST'),
-            CURLOPT_POSTFIELDS => $postfields,
-            CURLOPT_URL => $url
-        ));
+		return $format == 'array' ? json_decode($rs, true) : $rs;
+	}
 
-        return curl_exec($this->ch);
-    }
+	/**
+	 * Concatenate keys and values to url format and return url.
+	 *
+	 * @param   string $url
+	 * @param   array  $parameters
+	 * @return  string
+	 */
+	private function createUrl($url, $parameters)
+	{
+		$url .= '?' . http_build_query($parameters);
+
+		return $url;
+	}
+
+	/**
+	 * Executes request on link.
+	 *
+	 * @param   string $url
+	 * @param   string $method
+	 * @param   array  $postfields
+	 * @return  null|string
+	 */
+	private function request($url, $method = 'GET', $postfields = [])
+	{
+		switch (strtolower($method)) {
+			case "get":
+				return $this->ch->get($url);
+				break;
+			case "post":
+				return $this->ch->post($url, $postfields);
+				break;
+		}
+
+		return null;
+	}
+
+	/**
+	 * Returns SSL photo url.
+	 *
+	 * @param string $photoUrl
+	 * @return null|string
+	 */
+	public static function sslPhoto($photoUrl = null)
+	{
+		if (is_null($photoUrl))
+			return null;
+
+		$url = parse_url($photoUrl);
+
+		if ($url["scheme"] == "https")
+			return $photoUrl;
+
+		if ($url["host"] == "vk.com")
+			return sprintf("https://%s%s", $url["host"], $url["path"]);
+
+		preg_match("/cs(\d+)\.(.+)/", $url["host"], $domain);
+
+		return sprintf("https://pp.%s/c%s%s", $domain[2], $domain[1], $url["path"]);
+		/*
+		 * BEFORE:  http://cs625631.vk.me/v625631245/43f56/MCuFMclvN0U.jpg
+		 * AFTER:   https://pp.vk.me/c625631/v625631245/43f56/MCuFMclvN0U.jpg
+		 */
+	}
 
 }
-
-;
 
